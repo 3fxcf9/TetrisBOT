8d506c100827bfa9591a1deaf900f48b481f6890
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-04-07 09:41:28 +0200

    Add custom matrix iterator

---
+++ src/tetris.ts
@@ -0,0 +1,14 @@
+const all_tetraminos = require("./tetraminos.json");
+
+/**
+ * Matrix Iterator
+ * @param matrix A 2D matrix to iterate on
+ * @yields An array [value, row, col]
+ */
+function* matrixIterator(matrix: Array<Array<any>>): Generator<Array<any>> {
+	for (let _row = 0; _row < matrix.length; _row++) {
+		for (let _col = 0; _col < matrix[_row].length; _col++) {
+			yield [matrix[_row][_col], _row, _col];
+		}
+	}
+}
