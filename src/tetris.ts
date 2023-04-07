fb7848a6fc5aaddb5736017b95494441fd73cd49
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-04-07 09:42:26 +0200

    Add Tetramino class

--- src/tetris.ts
+++ src/tetris.ts
@@ -12,3 +12,50 @@ function* matrixIterator(matrix: Array<Array<any>>): Generator<Array<any>> {
 		}
 	}
 }
+
+class Tetramino {
+	x: number;
+	y: number;
+	type: string;
+	rotations: any;
+	current_rotation: number;
+	color: string;
+	projected_color: string;
+
+	constructor({ x = 0, y = 0, rotation = 0, type = "L" } = {}) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		this.current_rotation = rotation;
+
+		let tetramino_data = all_tetraminos[this.type];
+		this.rotations = tetramino_data.rotations;
+		this.color = tetramino_data.color;
+		this.projected_color = tetramino_data.projected_color;
+	}
+
+	get matrix() {
+		return this.rotations[this.current_rotation] as number[][];
+	}
+
+	setRotation(r: number) {
+		this.current_rotation = r;
+	}
+
+	rotate(dir: -1 | 1) {
+		this.current_rotation = (this.current_rotation + dir + 4) % 4; // TODO: Better rotation system
+	}
+
+	left() {
+		this.x -= 1;
+		return this;
+	}
+	right() {
+		this.x += 1;
+		return this;
+	}
+	down() {
+		this.y += 1;
+		return this;
+	}
+}
